68e33b829eaef224cb5118db52ddf4ca668c3c86
Luke Armstrong <ltarmstrong96@gmail.com>
2020-04-22 14:00:46 +1200

    Resume added
    Resume section added, unformatted

--- script.js
+++ script.js
@@ -1,26 +1,66 @@
-/* Nav Bar*/
+/******************************** Nav Bar*/
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {navBarStick()};
 
 // Get the navbar
 var navbar = document.getElementById("nav-container");
-
 var sticky = navbar.offsetTop;
 
 function navBarStick() {
-  if (window.pageYOffset >= sticky) {
+
+  if (window.pageYOffset >= sticky)
+   {
     navbar.classList.add("sticky")
-  } else {
+  }
+  else
+  {
     navbar.classList.remove("sticky");
   }
 }
 
 function goto(url)
 {
-window.location=url;
-
+  window.location=url;
 }
 
+/******************************** Contact buttons */
+function openLink(link) {
+  switch(link)
+  {
+    case 'instagram':
+      window.open("https://www.instagram.com/luke__armstrong/");
+      break;
+    case 'twitter':
+      window.open("https://twitter.com/LukeArm29378032");
+      break;
+    case 'github':
+      window.open("https://github.com/DevLukeArmstrong");
+      break;
+    case 'linkden':
+      window.open("https://www.linkedin.com/in/luke-armstrong96/");
+      break;
+    case 'email':
+      window.open("https://www.instagram.com/luke__armstrong/");
+      break;
+    default:
+      console.log("Unknown link: " + link);
+      break;
+  }
+}
+
+/******************************** Resume */
+var coll = document.getElementById("resume-button");
+var i;
+
+coll.addEventListener("click", function() {
+  this.classList.toggle("resume-active");
+  var content = this.nextElementSibling;
+  if (content.style.maxHeight){
+    content.style.maxHeight = null;
+  } else {
+    content.style.maxHeight = content.scrollHeight + "px";
+  }
+});
 
 function increaseResume(x) {
     document.getElementById(x).style.width = "100%";
